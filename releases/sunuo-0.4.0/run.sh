8353d93e4452e2ac52099a59ebe08aad0714da66
make <make@45309df1-3fee-0310-8168-e5b643bb5cc7>
2006-12-20 21:37:21 +0000

    release v0.4.0

---
+++ releases/sunuo-0.4.0/run.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+#  SunUO
+#  $Id$
+#
+#  (c) 2006 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+# copy SunUO.exe from bin/
+
+if ! [ -f SunUO.exe ]; then
+    if [ -f bin/mono/SunUO.exe ]; then
+        cp -v bin/mono/SunUO.exe .
+        if [ -f bin/mono/SunUO.exe.mdb ]; then
+            cp -v bin/mono/SunUO.exe.mdb .
+        fi
+    elif [ -f bin/w32/SunUO.exe ]; then
+        cp -v bin/w32/SunUO.exe .
+        rm -f SunUO.exe.mdb
+    fi
+fi
+
+# a few checks
+
+if ! [ -f SunUO.exe ]; then
+    echo "ERROR: SunUO.exe does not exist" >&2
+    exit 2
+fi
+
+if ! [ -f SunUO.exe.config ]; then
+    echo "Warning: SunUO.exe.config does not exist" >&2
+fi
+
+# prepare environment
+
+export LANG=C
+export LC_ALL=C
+
+unset DISPLAY XAUTHORITY
+
+export SUNUO_EXIT=99
+
+# our main loop
+while :; do
+    # start SunUO.exe
+    ${MONO:-mono} ${MONO_OPTS:---server --debug -O=all,-shared} SunUO.exe "$@"
+    STATUS=$?
+    echo "SunUO.exe exited with status $STATUS" >&2
+
+    # exit status 99 means "really exit, do not restart"
+    [ $STATUS == $SUNUO_EXIT ] && break
+
+    # sleep for a reasonable duration and restart
+    sleep 10
+    echo "Re-starting SunUO.exe"
+done
+
+echo "Leaving $0" >&2
---
+++ releases/sunuo-0.4.0/scripts/legacy/Accounting/Accounts.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Xml;
+
+namespace Server.Accounting
+{
+	public class Accounts
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		private static Hashtable m_Accounts = new Hashtable();
+
+		public static void Configure()
+		{
+			EventSink.WorldLoad += new WorldLoadEventHandler( Load );
+			EventSink.WorldSave += new WorldSaveEventHandler( Save );
+		}
+
+		static Accounts()
+		{
+		}
+
+		public static Hashtable Table
+		{
+			get
+			{
+				return m_Accounts;
+			}
+		}
+
+		public static Account GetAccount( string username )
+		{
+			return m_Accounts[username] as Account;
+		}
+
+		public static Account AddAccount( string user, string pass )
+		{
+			Account a = new Account( user, pass );
+			if ( m_Accounts.Count == 0 )
+				a.AccessLevel = AccessLevel.Administrator;
+
+			m_Accounts[a.Username] = a;
+
+			return a;
+		}
+
+		public static int GetInt32( string intString, int defaultValue )
+		{
+			try
+			{
+				return XmlConvert.ToInt32( intString );
+			}
+			catch
+			{
+				try
+				{
+					return Convert.ToInt32( intString );
+				}
+				catch
+				{
+					return defaultValue;
+				}
+			}
+		}
+
+		public static DateTime GetDateTime( string dateTimeString, DateTime defaultValue )
+		{
+			try
+			{
+				return XmlConvert.ToDateTime( dateTimeString );
+			}
+			catch
+			{
+				try
+				{
+					return DateTime.Parse( dateTimeString );
+				}
+				catch
+				{
+					return defaultValue;
+				}
+			}
+		}
+
+		public static TimeSpan GetTimeSpan( string timeSpanString, TimeSpan defaultValue )
+		{
+			try
+			{
+				return XmlConvert.ToTimeSpan( timeSpanString );
+			}
+			catch
+			{
+				return defaultValue;
+			}
+		}
+
+		public static string GetAttribute( XmlElement node, string attributeName )
+		{
+			return GetAttribute( node, attributeName, null );
+		}
+
+		public static string GetAttribute( XmlElement node, string attributeName, string defaultValue )
+		{
+			if ( node == null )
+				return defaultValue;
+
+			XmlAttribute attr = node.Attributes[attributeName];
+
+			if ( attr == null )
+				return defaultValue;
+
+			return attr.Value;
+		}
+
+		public static string GetText( XmlElement node, string defaultValue )
+		{
+			if ( node == null )
+				return defaultValue;
+
+			return node.InnerText;
+		}
+
+		public static void Save( WorldSaveEventArgs e )
+		{
+			string saveDirectory = Path.Combine(e.SaveDirectory, "Accounts");
+			if (!Directory.Exists(saveDirectory))
+				Directory.CreateDirectory(saveDirectory);
+
+			string filePath = Path.Combine(saveDirectory, "accounts.xml");
+
+			using ( StreamWriter op = new StreamWriter( filePath ) )
+			{
+				XmlTextWriter xml = new XmlTextWriter( op );
+
+				xml.Formatting = Formatting.Indented;
+				xml.IndentChar = '\t';
+				xml.Indentation = 1;
+
+				xml.WriteStartDocument( true );
+
+				xml.WriteStartElement( "accounts" );
+
+				xml.WriteAttributeString( "count", m_Accounts.Count.ToString() );
+
+				foreach ( Account a in Accounts.Table.Values )
+					a.Save( xml );
+
+				xml.WriteEndElement();
+
+				xml.Close();
+			}
+		}
+
+		public static void Load()
+		{
+			m_Accounts = new Hashtable( 32, 1.0f, CaseInsensitiveHashCodeProvider.Default, CaseInsensitiveComparer.Default );
+
+			string saveDirectory = Path.Combine(Core.Config.SaveDirectory, "Accounts");
+			string filePath = Path.Combine(saveDirectory, "accounts.xml");
+
+			if ( !File.Exists( filePath ) )
+				return;
+
+			log.Debug("loading accounts");
+
+			XmlDocument doc = new XmlDocument();
+			XmlReader reader = new XmlTextReader(filePath);
+
+			while (reader.Read()) {
+				if (reader.NodeType != XmlNodeType.Element ||
+					reader.Name != "account")
+					continue;
+
+				XmlElement account = (XmlElement)doc.ReadNode(reader);
+
+				try
+				{
+					Account acct = new Account( account );
+
+					m_Accounts[acct.Username] = acct;
+				}
+				catch(Exception e)
+				{
+					log.Error("Account instance load failed", e);
+				}
+			}
+		}
+	}
+}
